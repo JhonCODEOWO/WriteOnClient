f328c65555b795938d923018e3ca4747973794e7
David Nava <david.nava.garcia4@gmail.com>
2025-10-20 23:38:03 -0600

    feat(header): Rediseño del navbar con toggle de tema claro u oscuro
    -- Rediseño del header
    -- Se agregó una barra de busqueda  de notas: Solo por diseño  (aun no existe funcionalidad).
    -- Se agregó un nuevo boton en el header : 'Nueva Nota' el cual aun no tiene funcionalidad pero se tomará del mismo con el boton '+'.
    -- Configuración modo oscuro de Tailwind v4 con @custom-variant dark (&:where(.dark, .dark *));  en styles.css
    -- Creación de componente theme-toggle en global/components usando "ng g c global/components/theme-toggle --standalone" (sin módulo asociado (NgModule).)
    -- Utilización del botón component theme en el la pantalla de login.

--- src/app/auth/layout/auth-layout/auth-layout.component.ts
+++ src/app/auth/layout/auth-layout/auth-layout.component.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ThemeToggleComponent } from '../../../global/components/theme-toggle/theme-toggle.component';
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ThemeToggleComponent],
   templateUrl: './auth-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- src/app/global/components/navbar/navbar.component.ts
+++ src/app/global/components/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { AuthService } from '../../../auth/services/AuthService.service';
 import { Router } from '@angular/router';
-
+import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ThemeToggleComponent],
   templateUrl: './navbar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
