59410bf2f4446293ca66ef5c91f8826a79bee5e7
Jonathan Juárez Valera <jjv20618@gmail.com>
2025-11-25 13:25:06 -0600

    fix: Adjust notifications styles based in the device.

--- src/app/utils/notifications/components/notification-element/notification-element.component.ts
+++ src/app/utils/notifications/components/notification-element/notification-element.component.ts
@@ -12,12 +12,12 @@ interface NotificationElementClasses {
   selector: 'notification-element',
   imports: [NgClass],
   template: `
-    <div [ngClass]="classMap.get(notification().type)?.mainContent" [class.notification-enter]="notification().state === 'enter'" [class.notification-leave]="notification().state === 'leave'" role="alert" tabindex="-1" aria-labelledby="hs-bordered-success-style-label">
+    <div class="backdrop-blur-sm" [ngClass]="classMap.get(notification().type)?.mainContent" [class.notification-enter]="notification().state === 'enter'" [class.notification-leave]="notification().state === 'leave'" role="alert" tabindex="-1" aria-labelledby="hs-bordered-success-style-label">
           <div class="flex items-center">
             <div class="shrink-0">
               @switch (notification().type) {
                 @case (types.SUCCESS) {
-                  <span class="inline-flex justify-center items-center size-8 rounded-full border-4 border-teal-100 bg-teal-200 text-teal-800 dark:border-teal-900 dark:bg-teal-800 dark:text-teal-400">
+                  <span class="hidden md:inline-flex justify-center items-center size-8 rounded-full border-4 border-teal-100 bg-teal-200 text-teal-800 dark:border-teal-900 dark:bg-teal-800 dark:text-teal-400">
                   <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"></path>
                     <path d="m9 12 2 2 4-4"></path>
@@ -25,7 +25,7 @@ interface NotificationElementClasses {
                 </span>
                 }
                 @case (types.ERROR) {
-                          <span class="inline-flex justify-center items-center size-8 rounded-full border-4 border-red-100 bg-red-200 text-red-800 dark:border-red-500 dark:bg-red-800 dark:text-red-400">
+                          <span class="hidden md:inline-flex justify-center items-center size-8 rounded-full border-4 border-red-100 bg-red-200 text-red-800 dark:border-red-500 dark:bg-red-800 dark:text-red-400">
                           <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M18 6 6 18"></path>
                             <path d="m6 6 12 12"></path>
@@ -43,7 +43,7 @@ interface NotificationElementClasses {
                 </h3>
                 <!-- Text and date content -->
                 <div class="flex flex-col gap-y-2" [ngClass]="classMap.get(notification().type)?.textContent">
-                  <p class="text-sm break-words">
+                  <p class="text-xs md:text-sm break-words">
                     {{this.notification().message}}
                   </p>
                   <p class="text-xs">
@@ -72,8 +72,8 @@ export class NotificationElement {
 
   /** Map of tailwind classes to use in the template based on the Notification type */
   classMap: Map<TypeNotification, NotificationElementClasses> = new Map<TypeNotification, NotificationElementClasses>([
-    [TypeNotification.SUCCESS, {mainContent: 'bg-teal-50 border-t-2 border-teal-500 rounded-lg p-4 dark:bg-teal-800/80', textContent: 'text-teal-300'}],
-    [TypeNotification.ERROR, {mainContent: 'bg-red-50 border-s-4 border-red-500 p-4 dark:bg-red-800/80', textContent: 'text-red-300'}]
+    [TypeNotification.SUCCESS, {mainContent: 'bg-teal-50 border-t-2 border-teal-500 rounded-lg p-2 md:p-4 dark:bg-teal-800/80', textContent: 'text-teal-300'}],
+    [TypeNotification.ERROR, {mainContent: 'bg-red-50 border-s-4 border-red-500 p-2 md:p-4 dark:bg-red-800/80', textContent: 'text-red-300'}]
   ])
 
   /**
